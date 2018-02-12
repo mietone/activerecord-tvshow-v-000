c78cbd7d59f4f6450fc4a6cf60a68184de9334f2
Miwa <paintfx@gmail.com>
2018-02-12 07:10:55 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,11 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
 
   def self.most_popular_show
-    self.highest_rating.name
+    self.find_by(rating: self.highest_rating)
   end
 
 end
